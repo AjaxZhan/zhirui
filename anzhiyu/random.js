6fcb3e80f05d3388099fc9f86c629891108da2fb
CagurZhan <1655929802@qq.com>
2023-10-27 12:46:47 +0800

    Site updated: 2023-10-27 12:46:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/25/01-activity/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/25/01-activity/","2023/10/27/02-Vscode/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
